1d18bccc5233d22e46584f2f080d6a2c5da3f661
luizvnx <luizvnx@gmail.com>
2021-04-24 14:15:50 -0300

    :package: Funcionalidade do Menu

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.querySelector (".menu-lines").addEventListener("click",() => 
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+function goTo(id){
+    document.getElementById(id).scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+
